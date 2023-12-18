398316d4a17bac90ead0b1926fc60d2ae64242fa
abhishekPiperde-coditas <abhishek.piperde@coditas.com>
2023-12-18 11:28:38 +0530

    added axios call

--- app/modules/business-auth/business.routes.ts
+++ app/modules/business-auth/business.routes.ts
@@ -1,9 +1,11 @@
 import  {Request, Response, NextFunction, Router} from 'express';
 import { ResponseHandler } from '../../utility/response-handler';
 import businessService from './business.service';
+import axios from 'axios';
 
 export const BusinessRouter = Router();
-
+const facebookAppId = process.env.FACEBOOK_APP_ID;
+const redirectUri = 'https://9aa2-115-160-223-174.ngrok-free.app/callback';
 
 BusinessRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.render('index');
@@ -59,3 +61,52 @@ BusinessRouter.get("/get-campaign", async (req: Request, res: Response, next: Ne
         next(e);
     }
 })
+
+BusinessRouter.get('/auth/facebook', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const loginUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${facebookAppId}&redirect_uri=${redirectUri}&scope=ads_management,ads_read`;
+
+        console.log('Redirecting to Facebook Login URL:', loginUrl);
+
+        // Make an asynchronous request to the Facebook Login URL
+        const response = await axios.get(loginUrl);
+        console.log('Facebook Login URL Response:', response.data);
+
+        res.redirect(loginUrl);
+    } catch (error:any) {
+        console.error('Error during Facebook Login URL request:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+BusinessRouter.get('/callback', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        console.log('Callback received with query parameters:', req.query);
+
+        // Exchange the code for an access token
+        const tokenResponse = await axios.get('https://graph.facebook.com/v18.0/oauth/access_token', {
+            params: {
+                client_id: facebookAppId,
+                client_secret: process.env.FACEBOOK_APP_SECRET,
+                redirect_uri: redirectUri,
+                code: req.query.code,
+            },
+        });
+
+        console.log('Access Token Response:', tokenResponse.data);
+
+        const userInfoResponse = await axios.get('https://graph.facebook.com/me', {
+            params: {
+                access_token: tokenResponse.data.access_token,
+                fields: 'id,name,email', // Add the required fields
+            },
+        });
+
+        console.log('User Info Response:', userInfoResponse.data);
+
+        res.send('Facebook Login Callback');
+    } catch (error:any) {
+        console.error('Error during callback:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
